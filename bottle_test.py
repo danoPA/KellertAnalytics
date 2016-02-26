c32ae09dee55d77e9f3ae275e7af377171260a71
Scott Kellert <skellert@Scotts-MacBook-Air.local>
2016-02-26 08:16:35 -0600

    web app

---
+++ bottle_test.py
@@ -0,0 +1,61 @@
+from bottle import route, run, template, get
+from bottle import static_file, url
+import json
+
+def check_login(username, password):
+	with open("login.json", 'r') as infile:
+		logins = json.load(infile)
+		try:
+			return logins[username] == password
+		except KeyError:
+			return False
+
+@route('/')
+def home():
+	return template("index.html", url = url)
+
+
+@route('/static/<filename>', name = 'static')
+def send_static(filename):
+    return static_file(filename, root='static')
+
+# Static Routes
+# @get('/<filename:re:.*\.js>')
+# def javascripts(filename):
+#     return static_file(filename, root='static/js')
+
+# @get('/<filename:re:.*\.css>')
+# def stylesheets(filename):
+#     return static_file(filename, root='static/css')
+
+# @get('/<filename:re:.*\.(jpg|png|gif|ico)>')
+# def images(filename):
+#     return static_file(filename, root='static/img')
+
+# @get('/<filename:re:.*\.(eot|ttf|woff|svg)>')
+# def fonts(filename):
+#     return static_file(filename, root='static/fonts')
+
+
+from bottle import get, post, request # or route
+
+@get('/login') # or @route('/login')
+def login():
+    return '''
+        <form action="/login" method="post">
+            Username: <input name="username" type="text" />
+            Password: <input name="password" type="password" />
+            <input value="Login" type="submit" />
+        </form>
+    '''
+
+@post('/login') # or @route('/login', method='POST')
+def do_login():
+    username = request.forms.get('username')
+    password = request.forms.get('password')
+    if check_login(username, password):
+        return "<p>Your login information was correct.</p>"
+    else:
+        return "<p>Login failed.</p>"
+
+run(host='localhost', port=8080, debug=True)
