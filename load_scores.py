f72510f0831f04fab164e74ba1593cbb85043004
skellert <skellert22@gmail.com>
2016-03-30 12:34:34 -0500

    Masters

--- load_scores.py
+++ load_scores.py
@@ -2,18 +2,11 @@ from bs4 import BeautifulSoup
 import urllib2
 import datetime
 from collections import namedtuple
+import re
 
 
 def getContent(s,p = False):
-    check = False
-    n = ''
-    for i in str(s):
-        if i == '<' or i == ']':
-            check = False
-        if check:
-            n += i
-        if i == '>':
-            check = True
+    n = s.text
     if p:
         if n[0] == 'T':
             return int(n[1:])
@@ -35,113 +28,126 @@ def load_scores():
     content = page.read()
     soup = BeautifulSoup(content, 'html5lib')
 
-    #print soup.prettify()
-    soupTable = soup.findChildren('table',{'class':"tablehead leaderboard during"})
-    rows = soupTable[0].findChildren(['th', 'tr'])
-
     standings = {
         "Kellert":[
-            "Jason Day",
-            "Henrik Stenson",
-            "Kevin Chappell",
-            "Justin Rose",
-            "Jamie Lovemark",
-            "Troy Merritt"
+            "",
+            "",
+            "",
+            "",
+            "",
+            ""
         ],
         "Keesee":[
-            "Jason Day",
-            "Paul Casey",
-            "Kevin Chappell",
-            "Justin Rose",
-            "Jamie Lovemark",
-            "Troy Merritt"
+            "",
+            "",
+            "",
+            "",
+            "",
+            ""
         ],
         "Shane":[
-            "Jason Day",
-            "Chris Wood",
-            "Kevin Chappell",
-            "Justin Rose",
-            "Jamie Lovemark",
-            "Troy Merritt"
+            "",
+            "",
+            "",
+            "",
+            "",
+            ""
         ],
         "Rankin":[
-            "Jason Day",
-            "Henrik Stenson",
-            "Kevin Chappell",
-            "Justin Rose",
-            "Brian Harman",
-            "Troy Merritt"
+            "",
+            "",
+            "",
+            "",
+            "",
+            ""
         ],
         "Trent":[
-            "Jason Day",
-            "Henrik Stenson",
-            "Kevin Chappell",
-            "Justin Rose",
-            "Jamie Lovemark",
-            "Troy Merritt"
+            "",
+            "",
+            "",
+            "",
+            "",
+            ""
         ],
         "Bergman":[
-            "Jason Day",
-            "Henrik Stenson",
-            "Keegan Bradley",
-            "Justin Rose",
-            "Jamie Lovemark",
-            "Troy Merritt"
+            "",
+            "",
+            "",
+            "",
+            "",
+            ""
         ],
-        "Paul":[
-            "Jason Day",
-            "Henrik Stenson",
-            "Kevin Chappell",
-            "Billy Horschel",
-            "Jamie Lovemark",
-            "Troy Merritt"
+        "Grayhack":[
+            "",
+            "",
+            "",
+            "",
+            "",
+            ""
         ],
         "Moops":[
-            "Jason Day",
-            "Henrik Stenson",
-            "Kevin Chappell",
-            "Justin Rose",
-            "Kyle Reifers",
-            "Troy Merritt"
+            "",
+            "",
+            "",
+            "",
+            "",
+            ""
         ],
         "Sample":[
-            "Jason Day",
-            "Will Wilcox",
-            "Kevin Chappell",
-            "Justin Rose",
-            "Jamie Lovemark",
-            "Troy Merritt"
+            "",
+            "",
+            "",
+            "",
+            "",
+            ""
         ],
         "Day":[
-            "Jason Day",
-            "Henrik Stenson",
-            "Kevin Chappell",
-            "Justin Rose",
-            "Zac Blair",
-            "Ernie Els"
+            "",
+            "",
+            "",
+            "",
+            "",
+            ""
         ]
     }
 
-    places = {}
-    cut = soupTable[0].find("tr",{"class":"cut-line"})
+    #print soup.prettify()
 
-    for i, row in enumerate(rows):
-        if row == cut:
-            print i, "cut"
-            cutPlace = int(place)
-        try:
-            place = getContent(row.find_all("td",{"class":"textcenter"})[0], p = True)
-            name = row.find_all("a")
-            score = getContent(row.find_all("td",{"class":"textcenter"})[3], p = True)
-            try:
-                hole = int(getContent(row.find_all("td",{"class":"textcenter"})[5], p = True))
-            except:
-                hole = getContent(row.find_all("td",{"class":"textcenter"})[5], p = True)
-            if place == "-" or hole == "N/A":
-                place = cutPlace + 5
-            places[getContent(name)] = {"place":int(place),"score":score,"hole":hole}
-        except:
-            pass
+    if len(soup.findChildren('table',{'class':"tablehead leaderboard before"})) == 1:
+
+        soupTable = soup.findChildren('table',{'class':"tablehead leaderboard before"})
+        headers = soupTable[0].findChildren('th')
+        rows = soupTable[0].findChildren('tr', {'class':re.compile(r"^(evenrow|oddrow)$")})
+        tableheaders = [header.text.replace(' ','') for header in headers]
+
+        places = {}
+        table_tuple = namedtuple("table_tuple", tableheaders)
+        for i, row in enumerate(rows):
+            tt = [j.text for j in row.findAll('td')]
+            tt = table_tuple(*tt)
+            places[tt.PLAYER] = tt
+
+    if len(soup.findChildren('table',{'class':"tablehead leaderboard during"})) == 1:
+
+        soupTable = soup.findChildren('table',{'class':"tablehead leaderboard during"})
+        headers = soupTable[0].findChildren('th')
+        rows = soupTable[0].findChildren('tr', {'class':re.compile(r"^(evenrow|oddrow)$")})
+        tableheaders = [header.text.replace(' ','') for header in headers]
+        cutplace = 1000
+        cutcheck = True
+
+        places = {}
+        table_tuple = namedtuple("table_tuple", tableheaders)
+        for i, row in enumerate(rows):
+            tt = [j.text for j in row.findAll('td')]
+            if cutcheck and tt[tableheaders.index("POS")] == "CUT":
+                cutplace = i + 5
+                cutcheck = False
+                tt[tableheaders.index("POS")] = cutplace
+            if "T" in tt[tableheaders.index("POS")]:
+                tt[tableheaders.index("POS")] = tt[tableheaders.index("POS")][1:]
+            tt = table_tuple(*tt)
+            places[tt.PLAYER] = tt
 
     standing_tuple = namedtuple("standing_tuple", ["name","score"])
     players_tuple = namedtuple("players_tuple", ["player","hole","score","place"])
@@ -153,9 +159,17 @@ def load_scores():
         individual_standings[name] = []
         score = 0
         for player in players:
-            x = [player]+places[player].values()
+            x = []
+            for field in players_tuple._fields:
+                try:
+                    x.append(getattr(places[player], field))
+                except:
+                    x.append("")
             individual_standings[name].append(players_tuple(*x))
-            score += int(places[player]["place"])
+            try:
+                score += int(places[player].SCORE)
+            except:
+                score += 0
         total_standings.append(standing_tuple(name,score))
 
     total_standings = sorted(total_standings, key=lambda x: x.score,reverse=False)
